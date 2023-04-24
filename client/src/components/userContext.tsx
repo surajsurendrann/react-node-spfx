26c776c63c9c95f2c5a966dea1e0a15050765a5a
suraj <surajsurendran1910@gmail.com>
2023-04-24 11:46:10 +0530

    editted userContext

--- client/src/components/userContext.tsx
+++ client/src/components/userContext.tsx
@@ -20,10 +20,8 @@ export interface User {
 
 interface UserContextType {
   users: User[];
-  // upUsers: number;
   addUser: (newUser: User) => void;
   deleteUser: (Id: number | undefined) => void;
-  // handleUpdate: (Id: number) => void;
   updateUser: (updatedUser: User) => void;
 }
 
@@ -33,17 +31,13 @@ interface UserProviderProps {
 
 export const UserContext = createContext<UserContextType>({
   users: [],
-  // upUsers: null,
   addUser: () => {},
   deleteUser: () => {},
-  // handleUpdate: () => {},
   updateUser: () => {},
 });
 
 export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
   const [users, setUsers] = useState<User[]>([]);
-  // const [upUsers, setUpUsers] = useState<number>(null);
-  // const [userId, setUserId] = useState<number>();
 
   //Get users
   useEffect(() => {
@@ -71,16 +65,6 @@ export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
     );
     console.log(response);
   };
-
-  //Update
-
-  // const handleUpdate = async (Id: number) => {
-  //   setUserId(Id);
-  //   const items: any = await sp.web.lists
-  //     .getByTitle("My List")
-  //     .items.getById(Id)();
-  //   setUpUsers(items);
-  // };
 
   const updateUser = async (newUser: User) => {
     const response = await axios.put(
@@ -128,113 +112,3 @@ export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
     </UserContext.Provider>
   );
 };
-
-/* eslint-disable @typescript-eslint/no-empty-function */
-// /* eslint-disable @typescript-eslint/explicit-function-return-type */
-// /* eslint-disable @typescript-eslint/no-floating-promises */
-// import * as React from "react";
-// import { createContext, useState, useEffect } from "react";
-// import { sp } from "@pnp/sp/presets/all";
-
-// export interface IListItem {
-//   Id: number;
-//   Title: string;
-//   Email: string;
-//   Designation: string;
-// }
-
-// interface SharePointListContextType {
-//   listItems: IListItem[];
-//   addListItem: () => void;
-//   deleteListItem: (itemId: number) => void;
-//   updateListItem: (itemId: number, title: string, description: string) => void;
-// }
-
-// export const SharePointListContext = createContext<SharePointListContextType>({
-//   listItems: [],
-//   addListItem: () => {},
-//   deleteListItem: () => {},
-//   updateListItem: () => {},
-// });
-
-// export const SharePointListProvider: React.FC = ({ children }) => {
-//   const [listItems, setListItems] = useState<IListItem[]>([]);
-
-//   useEffect(() => {
-//     sp.web.lists
-//       .getByTitle("Users")
-//       .items.select("Title", "Description")
-//       .get()
-//       .then((items) => {
-//         setListItems(items);
-//       });
-//     console.log(listItems);
-//   }, []);
-
-//   const addListItem = () => {
-//     sp.web.lists
-//       .getByTitle("users")
-//       .items.add({
-//         Title: "New Item",
-//         Description: "New item added using React and TypeScript",
-//       })
-//       .then(() => {
-//         sp.web.lists
-//           .getByTitle("ListName")
-//           .items.select("Id", "Title", "Description")
-//           .get()
-//           .then((items) => {
-//             setListItems(items);
-//           });
-//       });
-//   };
-
-//   const deleteListItem = (itemId: number) => {
-//     sp.web.lists
-//       .getByTitle("ListName")
-//       .items.getById(itemId)
-//       .delete()
-//       .then(() => {
-//         const updatedItems = listItems.filter((item) => item.Id !== itemId);
-//         setListItems(updatedItems);
-//       });
-//   };
-
-//   const updateListItem = (
-//     itemId: number,
-//     title: string,
-//     description: string
-//   ) => {
-//     sp.web.lists
-//       .getByTitle("ListName")
-//       .items.getById(itemId)
-//       .update({
-//         Title: title,
-//         Description: description,
-//       })
-//       .then(() => {
-//         sp.web.lists
-//           .getByTitle("ListName")
-//           .items.select("Id", "Title", "Description")
-//           .get()
-//           .then((items) => {
-//             setListItems(items);
-//           });
-//       });
-//   };
-
-//   const contextValue: SharePointListContextType = {
-//     listItems,
-//     addListItem,
-//     deleteListItem,
-//     updateListItem,
-//   };
-
-//   return (
-//     <SharePointListContext.Provider value={contextValue}>
-//       {children}
-//     </SharePointListContext.Provider>
-//   );
-// };
-
-// export default SharePointListProvider;
